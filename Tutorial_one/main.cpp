36351765a8edc8dfd949b69369655483bc24f770
sangzelahnamte <sangaguitarist@gmail.com>
2025-02-09 00:41:11 -0800

    added debug info to main.cpp file

--- Tutorial_one/main.cpp
+++ Tutorial_one/main.cpp
@@ -1,9 +1,11 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    qInfo() << "I`ve made some changes to the source code here";
 
     return a.exec();
 }
