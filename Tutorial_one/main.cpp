fabf3153b98ae725b8c30fc216659811f19553ee
sangzelahnamte <sangaguitarist@gmail.com>
2025-02-09 00:49:04 -0800

    added user input program to main.cpp

--- Tutorial_one/main.cpp
+++ Tutorial_one/main.cpp
@@ -1,11 +1,17 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     qInfo() << "I`ve made some changes to the source code here";
+
+    std::string name_var = "";
+    std::cout << "Write your name: ";
+    std::cin >> name_var;
+    qInfo() << "Name: " << name_var;
 
     return a.exec();
 }
